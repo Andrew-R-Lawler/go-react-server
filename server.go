e1ded299a9a5f6f0180574ef7818f3239769bead
andrew-r-lawler <andrew.r.lawler@gmail.com>
2025-02-03 17:27:36 -0600

    created init function that loads environment variables, gin now runs on the port specified in .env file

--- server.go
+++ server.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
 
 func main() {
 	r := gin.Default()
-
 	r.Use(static.Serve("/", static.LocalFile("./client/dist", true)))
 	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "bringus brongus give us dongus")
+		c.String(200, "pong")
+		log.Println("GET /ping called")
+	})
+	r.GET("/pong", func(c *gin.Context) {
+		c.String(200, "new POST who dis")
+		log.Println("POST /pong called")
 	})
 
-	r.Run(":8080")
+	r.Run()
 }
