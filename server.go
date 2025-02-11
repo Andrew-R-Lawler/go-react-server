c3960f784de900c9554a9257033aa37f6ebb9af8
andrew-r-lawler <andrew.r.lawler@gmail.com>
2025-02-10 19:03:44 -0600

    built in postgres database connection that connects based on environment variables found in .env

--- server.go
+++ server.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"log"
+	"fmt"
+	"database/sql"
+	"os"
 
+	_ "github.com/lib/pq"
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -16,6 +20,31 @@ func init() {
 }
 
 func main() {
+	// define connection string variables
+	dbuser := os.Getenv("DB_USER")
+	dbpassword := os.Getenv("DB_PASSWORD")
+	dbhost := os.Getenv("DB_HOST")
+	dbname := os.Getenv("DB_NAME")
+
+	// define connection string
+	connStr := fmt.Sprintf("user=%s password=%s host=%s dbname=%s sslmode=disable", dbuser, dbpassword, dbhost, dbname)
+		
+	// open the connection
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// test the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Successfully connected to the database")
+	
+
+	// gin router defined
 	r := gin.Default()
 	r.SetTrustedProxies(nil) // change this to a slice of strings containing trusted proxy IPs for production
 	r.Use(static.Serve("/", static.LocalFile("./client/dist", true)))
@@ -24,5 +53,6 @@ func main() {
 		log.Println("GET /ping called")
 	})
 
+	// run gin server
 	r.Run()
 }
