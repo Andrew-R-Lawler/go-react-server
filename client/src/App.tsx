6ed135b00b734dba32ec6b0baa72a85035960049
andrew-r-lawler <andrew.r.lawler@gmail.com>
2025-02-12 12:57:05 -0600

    integrated chakra petch google font into tailwindcss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -18,13 +18,13 @@ function App() {
     <div className="flex-container">
         <div className="flex-item">
         <h1 className="header1">To-Do List</h1>
-        <div className="flex w-full max-w-sm items-center space-x-0 border rounded-md overflow-hidden focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-orange-400">
+        <div className="flex w-full max-w-sm items-center space-x-0 border-none rounded-md overflow-hidden focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-orange-400">
       <Input
         type="text"
         value={inputValue}
         onChange={handleChange}
         placeholder="Add an item..."
-        className="border-none focus-visible:ring-0 focus-visible:ring-offset-0"
+        className="font-chakra border-none focus-visible:ring-0 focus-visible:ring-offset-0"
       />
       <Button
         className="bg-gradient-to-r from-orange-400 to-green-500 hover:from-orange-500 hover:to-green-600 text-white border-none rounded-none p-2 w-10 h-10 flex-shrink-0"
