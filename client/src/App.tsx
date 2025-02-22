f46b9efa4f8013bb07bea8cdb5156d0efe42c035
andrew-r-lawler <andrew.r.lawler@gmail.com>
2025-02-22 16:16:19 -0600

    installed react-router and created routes for the to-do and login components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,30 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Todo from './components/todo'
+import Login from './components/login'
 
 function App() {
-    
+
   return (
-    <div className="flex-container">
-        <Todo />
-    </div>
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/todo">Todo</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/todo" element={<Todo />} />
+        </Routes>
+      </div>
+    </Router>
   )
 }
 
