e93e9e310080ec5efe1e1cf41302405dd7e106e4
andrew-r-lawler <andrew.r.lawler@gmail.com>
2025-02-23 15:02:45 -0600

    added homepage component, set it to the apps root route, moved login to new login route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Todo from './components/todo'
 import Login from './components/login'
+import HomePage from './components/homepage'
 import logo from '../src/assets/icons8-checkmark.svg'
 
 function App() {
@@ -12,20 +13,23 @@ function App() {
         <nav className='nav-bar bg-black'>
           <ul className='nav-list'>
             <li className='nav-item'>
+                <Link to="/">
                 <img src={logo} width="40" height="40"/>
+                </Link>
             </li>
             <li className='nav-item'>
                 <Link to="/todo">Todo</Link>
             </li>
             <li className='nav-item'>
-                <Link to="/">Login</Link>
+                <Link to="/login">Login</Link>
             </li>
           </ul>
         </nav>
 
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/todo" element={<Todo />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </Router>
---
+++ client/src/components/homepage.tsx
@@ -0,0 +1,12 @@
+import '../App.css'
+
+function HomePage() {
+
+    return (
+        <div className='flex-container'>
+            <h1 className='chakra-petch-regular text-white'>Home</h1>
+        </div>
+    )
+}
+
+export default HomePage
