797c346ffef5c6443d1b9ce862f9dcdca33f80be
Andrew-R-Lawler <andrew.r.lawler@gmail.com>
2025-03-29 20:42:30 -0500

    forgotten password form successfully takes in email address from form data

--- client/src/components/forgotpassword.tsx
+++ client/src/components/forgotpassword.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 import '../App.css'
 import { Button } from './ui/button';
 import { Input } from './ui/input';
-import axios from 'axios'
+// import axios from 'axios'
 import { Label } from "@/components/ui/label";
 import {
   Card,
@@ -14,20 +14,30 @@ import {
 } from "./ui/card"
 
 function ForgotPassword() {
-    const [inputValue, setInputValue] = useState<string>('');
+    // const [inputValue, setInputValue] = useState<string>('');
 
-    const handleForgotPassword = async (e: React.FormEvent) => {
-        e.preventDefault()
-        try {
-            axios.post('/api/user/forgotpassword')
-        } catch (err) {
-            console.error(err)
+    // const handleForgotPassword = async (e: React.FormEvent) => {
+    //    e.preventDefault()
+    //    try {
+    //        axios.post('/api/user/forgotpassword')
+    //    } catch (err) {
+    //        console.error(err)
+    //    }
+    // }
+
+    // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //    setInputValue(event.target.value);
+    // };
+    
+    const handleForgotPassword = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const email = formData.get('email')
+        const postData = {
+            email: email
         }
+        console.log(postData)
     }
-
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setInputValue(event.target.value);
-    };
 
     return (
         <div className='flex-container chakra-petch-regular'>
@@ -37,17 +47,17 @@ function ForgotPassword() {
                 <CardDescription>Enter your email address and we'll send you a link to reset your password.</CardDescription>
             </CardHeader>
             <CardContent>
-            <form>
+            <form onSubmit={handleForgotPassword}>
             <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="name">E-Mail Address</Label>
-              <Input id="email" placeholder="name@example.com" className='border-none'/>
+              <Input id="email" name='email' placeholder="name@example.com" className='border-none'/>
             </div>
             </div>
             </form>
             </CardContent>
             <CardFooter className="flex justify-end">
-                <Button className='w-full'>Send Reset Link</Button>
+                <Button className='w-full' type='submit'>Send Reset Link</Button>
             </CardFooter>
         </Card>
         </div>
