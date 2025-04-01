45ed5f05e3fb5b78a61daa0c34819ecbc1ae2dd5
Andrew-R-Lawler <andrew.r.lawler@gmail.com>
2025-04-01 13:46:43 -0500

    updated password reset form to have conditional input rendering according to input validation

--- client/src/components/password-reset.tsx
+++ client/src/components/password-reset.tsx
@@ -1,5 +1,6 @@
 import '../App.css'
-// import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import React, { useState } from 'react'
 import { Button } from './ui/button';
 import { Input } from './ui/input';
 import { Label } from "@/components/ui/label";
@@ -13,12 +14,45 @@ import {
 } from "./ui/card"
 
 function PasswordReset() {
-//    const { token } = useParams();
+    const { token } = useParams();
+    const [isLoading, setIsLoading] = useState(false)
+    const [isValid, setIsValid] = useState(false)
+
+    const handlePasswordReset = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newPassword = formData.get('New Password')
+        const confirmPassword = formData.get('Confirm Password')
+        const postData = {
+            newPassword: newPassword,
+            confirmPassword: confirmPassword,
+            token: token,
+        }
+        setIsLoading(true)
+        try {
+            setIsLoading(false)
+        } catch (err) {
+            console.error(err)
+        }
+        console.log(postData)
+    }
+
+    const handleInputValidation = (event: React.FormEvent<HTMLFormElement>) => {
+        const formData = new FormData(event.currentTarget)
+        const newPassword = formData.get('New Password')
+        const confirmPassword = formData.get('Confirm Password')
+        if (newPassword === confirmPassword) {
+            setIsValid(true)
+        }
+        if (newPassword !== confirmPassword) {
+            setIsValid(false)
+        }
+    }
 
     return (
         <div className='flex-container chakra-petch-regular'>
         <Card className="w-[350px] bg-stone-600 border-none text-white">
-        <form>
+        <form onSubmit={handlePasswordReset} onChange={handleInputValidation}>
             <CardHeader>
                 <CardTitle>Reset password</CardTitle>
                 <CardDescription>Please enter your new password.</CardDescription>
@@ -26,16 +60,16 @@ function PasswordReset() {
             <CardContent>
             <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5 pt-2">
-              <Label htmlFor="new-password">New Password</Label>
+              <Label htmlFor="new-password" className='pt-2'>New Password</Label>
               <Input id="new-password" name='New Password' type='password' className='border-none'/>
-              <Label htmlFor="confirm-password">Confirm Password</Label>
-              <Input id="confirm-password" name='Confirm Password' type='password' className='border-none'/>
+              <Label htmlFor="confirm-password" className='pt-2'>Confirm Password</Label>
+              <Input id="confirm-password" name='Confirm Password' type='password' className={isValid ? 'border-green-600' : 'border-red-600'}/>
             </div>
             </div>
             </CardContent>
             <CardFooter className="flex justify-end pt-2">
                 <Button className='w-full' type='submit'>
-                    Reset Password
+                    {isLoading ? "Resetting Password..." : "Reset Password"}
                 </Button>
             </CardFooter>
             </form>
