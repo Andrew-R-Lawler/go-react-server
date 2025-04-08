cc91b94c0afc45ef2c5e51774051d8222b94fe55
Andrew-R-Lawler <andrew.r.lawler@gmail.com>
2025-04-08 18:42:42 -0500

    added add products route and component

---
+++ client/src/components/add-products.tsx
@@ -0,0 +1,12 @@
+import '../App.css'
+
+function AddProducts() {
+
+    return (
+        <div className='flex-container'>
+            <h1 className='chakra-petch-regular text-white'>Add Products</h1>
+        </div>
+    )
+}
+
+export default AddProducts
