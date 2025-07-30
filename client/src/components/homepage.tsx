48ac72937bac2338a1da9caf573a299f13aea787
Andrew-R-Lawler <andrew.r.lawler@gmail.com>
2025-07-30 17:34:18 -0500

    implemented responsive UI for the home page

--- client/src/components/homepage.tsx
+++ client/src/components/homepage.tsx
@@ -1,10 +1,24 @@
 import '../App.css'
+import { useEffect, useState } from 'react';
 
 function HomePage() {
     
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+    useEffect(() => {
+        const handleResize = () => setIsMobile(window.innerWidth < 768);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []); 
+
     return (
         <div className='flex-container'>
+        { isMobile && 
+            <h1 className='chakra-petch-regular text-white'>Mobile Home</h1>
+        }
+        { !isMobile && 
             <h1 className='chakra-petch-regular text-white'>Home</h1>
+        }
         </div>
     )
 }
