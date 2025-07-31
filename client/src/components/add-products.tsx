fb0b0f901b274a27b5794b7a7e37f716f820aaa1
Andrew-R-Lawler <andrew.r.lawler@gmail.com>
2025-07-31 14:43:21 -0500

    moved code for popover to allow better state control of edit product popover

--- client/src/components/add-products.tsx
+++ client/src/components/add-products.tsx
@@ -9,6 +9,7 @@ import { Card, CardContent, CardFooter, CardHeader, CardTitle, CardDescription }
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import axios from 'axios'
 import { Trash2, Edit2 } from "lucide-react"
+import  MobilePopover from './mobile-popover'
 import {
     Table,
     TableBody,
@@ -90,8 +91,8 @@ function AddProducts() {
         const form = document.getElementById('edit-product-form') as HTMLFormElement
         fetchProducts()
         setIsLoading(false)
-        form.reset()
         setOpen(false)
+        form.reset()
     }
 
     const handleAddProduct = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -148,70 +149,17 @@ function AddProducts() {
                             </CardContent>
                             <CardContent>
                                 <CardTitle>Price</CardTitle>
-                                <p>{product.price}</p>
+                                <p>{product.price.toFixed(2)}</p>
                             </CardContent>
                             <CardFooter className='flex flex-col'>
-                                <Popover open={open} onOpenChange={setOpen}>
-                                    <PopoverTrigger asChild>
-                                        <Button className='w-full' onClick={() => setOpen(!open)}>
-                                            Edit Product
-                                        </Button>
-                                    </PopoverTrigger>
-                                    <PopoverContent className='text-white m-3 p-0 w-full bg-stone-600'>
-                                        <Card className="bg-stone-600 border-none text-white">
-                                            <form id='edit-product-form' onSubmit={(event) => handleEditProduct(event, product)} autoComplete='on' >
-                                                <CardHeader>
-                                                    <CardTitle className='pb-3'>Edit Product</CardTitle>
-                                                </CardHeader>
-                                                <CardContent>
-                                                    {error &&
-                                                        <Alert variant="destructive" className='border-red-600 text-red-600 p-2 my-2 bg-red-300'>
-                                                            <AlertCircle className="h-4 w-4" />
-                                                            <AlertTitle>Error</AlertTitle>
-                                                            <AlertDescription><p className='text-red-600'>{error}</p></AlertDescription>
-                                                        </Alert>
-                                                    }
-                                                    <div>
-                                                        <div>
-                                                            <Label htmlFor="name">Name</Label>
-                                                            <Input defaultValue={product.name} id="name" name='name' type='text' placeholder="Lavender Lemon Shampoo Bar" className='border-none justify-self-center' disabled={isLoading} required />
-                                                        </div>
-                                                        <div>
-                                                            <Label htmlFor="image-url">Image URL</Label>
-                                                            <Input defaultValue={product.image_url} id="image-url" name='image-url' type='url' placeholder="google.com" className='border-none justify-self-center' disabled={isLoading} required />
-                                                        </div>
-                                                        <div>
-                                                            <Label htmlFor="description">Description</Label>
-                                                            <Textarea defaultValue={product.description} id="description" name='description' placeholder="Description here..." className='border-none justify-self-center bg-stone-700 ml-2' disabled={isLoading} required />
-                                                        </div>
-                                                        <div>
-                                                            <Label htmlFor="price">Price</Label>
-                                                            <Input defaultValue={product.price} id="price" name='price' type='number' step="0.01" min="0" placeholder="Enter Price" className='border-none justify-self-center' disabled={isLoading} required />
-                                                        </div>
-                                                        <div>
-                                                            <Label htmlFor="stock-quantity">Stock Quantity</Label>
-                                                            <Input defaultValue={product.stock_quantity} id="stock-quantity" name='stock-quantity' type='number' min='0' placeholder='0' className='border-none justify-self-center' disabled={isLoading} required />
-                                                        </div>
-                                                    </div>
-                                                </CardContent>
-                                                <CardFooter className="pt-2 justify-center">
-                                                    <div className='w-[75%]'>
-                                                        <Button className='w-full' type='submit' disabled={isLoading}>
-                                                            {isLoading ? "Updating Product..." : "Update Product"}
-                                                        </Button>
-                                                    </div>
-                                                </CardFooter>
-                                            </form>
-                                        </Card>
-                                    </PopoverContent>
-                                </Popover>
-                                <Button
+                                <MobilePopover product={product} fetchProducts={fetchProducts} />
+                                <button
                                     className="bg-stone-900 text-white border-none p-2 mt-2 w-full"
                                     aria-label={`Delete`}
                                     onClick={() => handleDeleteProduct(product)}
                                 >
                                     Delete Product
-                                </Button>
+                                </button>
                             </CardFooter>
                         </Card>
                     ))}
